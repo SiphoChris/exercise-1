74ceb507ba0a110ab80a7b246ecad22a2b1fb4ee
SiphoChris <siphonkebe@gmail.com>
2024-05-14 14:09:52 +0200

    task submitted

---
+++ app.js
@@ -0,0 +1,21 @@
+// question 1
+let name = 'Sipho'
+let surname = 'Nkebe'
+
+// question 2
+let fullName = name + ' ' + surname
+
+// question 3
+let  numberVar = 5
+
+// question 4
+console.log(numberVar*5)
+
+// question 5
+const firstName = 'Sipho'
+
+firstName = 'Nkebe'
+
+// Uncaught TypeError: Assignment to constant variable.
+
+// yeah I got an error, because constant variables cannot be reassigned values
